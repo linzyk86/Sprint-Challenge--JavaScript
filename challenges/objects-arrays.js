ff04c8f1c90821681dd3058c6dd74de7b935034b
Lindsay W <lwhite5314@gmail.com>
2020-03-20 19:56:37 -0400

    finished functions/arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -76,7 +76,11 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, log the result. */
-const universities = [];
+let universities = graduates.map((arrItem)=>{
+  return arrItem.university;
+
+})
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -85,11 +89,22 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+let contactInfo = graduates.map((arrItem)=>{
+  return `${arrItem.first_name}, ${arrItem.email}`;
+
+})
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+  let unisWithUni = [];
+
+let uni = function(universities){
+  let newArr = universities.includes('Uni');
+  newArr.push(unisWithUni);
+}
+
+
 console.log(unisWithUni);
 
 
